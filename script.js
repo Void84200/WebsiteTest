0da88e010b523b236b4198b8255ae09c7d599a40
Void84200 <amikomowyobili15@gmail.com>
2024-10-30 20:04:48 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/source/yarhm/1.18/yarhm.lua"))()
-
 -- YARHM now loads with GitHub, and might be permanently. To load from the official website:
